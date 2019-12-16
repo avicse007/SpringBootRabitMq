aca99033a62dd81fdadae667a0c4bc787f382169
Avinash Kumar Singh <avkumars@adobe.com>
2019-12-17 04:09:12 +0530

    adding the producer

---
+++ src/test/java/com/avinash/room/cleaner/producer/RoomCleanerProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avinash.room.cleaner.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomCleanerProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
